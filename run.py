bec16cc801e513bfac70c52bffe6d3fa531ba0f0
Endorocket <43682886+Endorocket@users.noreply.github.com>
2020-04-19 19:03:41 +0200

    Add config files for heroku deployment 5

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from code.app import app
-from code.config.db import db
-from code.config.bcrypt import bcrypt
+from code1.app import app
+from code1.config.db import db
+from code1.config.bcrypt import bcrypt
 
 db.init_app(app)
 bcrypt.init_app(app)
