84df3152858e4a93bfe1b15c2c9b7026f7e1331d
Endorocket <43682886+Endorocket@users.noreply.github.com>
2020-04-19 23:16:30 +0200

    Grant and revoke event admin

--- code1/app.py
+++ code1/app.py
@@ -7,7 +7,7 @@ from flask_restful import Api
 from config.bcrypt import bcrypt
 from config.db import db, DevelopmentConfig, LocalConfig
 from resources.event import Event, CreateEvent, EventList
-from resources.participant import InvitationByUsername, JoinByLink, RemoveUser
+from resources.participant import InvitationByUsername, JoinByLink, RemoveUser, GrantAdmin, RevokeAdmin
 from resources.spotify_login import SpotifyLogin, SpotifyAuthorize
 from resources.user import UserRegister, User, UserLogin
 
@@ -34,6 +34,8 @@ api.add_resource(EventList, "/events")
 api.add_resource(CreateEvent, "/event")
 api.add_resource(InvitationByUsername, "/event/<event_id>/invite")
 api.add_resource(RemoveUser, "/event/<event_id>/remove-user")
+api.add_resource(GrantAdmin, "/event/<event_id>/grant-admin")
+api.add_resource(RevokeAdmin, "/event/<event_id>/revoke-admin")
 api.add_resource(JoinByLink, "/join-event")
 
 db.init_app(app)
--- code1/resources/participant.py
+++ code1/resources/participant.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, reqparse
 
 from enums.role import Role
 from models.event import EventModel
+from models.participant import ParticipantModel
 from models.user import UserModel
 
 
@@ -95,3 +96,69 @@ class RemoveUser(Resource):
             return {"message": "Event removed because last admin was removed"}, 200
 
         return {"message": "User removed from an event"}, 200
+
+
+class GrantAdmin(Resource):
+    @classmethod
+    @jwt_required
+    def get(cls, event_id: str):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        data = parser.parse_args()
+
+        current_userid = get_jwt_identity()
+        current_user = UserModel.find_by_id(bson.ObjectId(current_userid))
+        if not current_user:
+            return {"message": "Current user not found"}, 403
+
+        user_to_update = UserModel.find_by_username(data['username'])
+        if not user_to_update:
+            return {"message": "User you want to add not found"}, 403
+
+        event = EventModel.find_by_id_and_admin_id(bson.ObjectId(event_id), current_user.id)
+        if not event:
+            return {"message": "Event with admin as current user not found"}, 403
+
+        participant_to_update: ParticipantModel = next(
+            filter(lambda participant: participant.user_id == user_to_update.id, event.participants), None)
+        if not participant_to_update:
+            return {"message": "User you want to grant admin is not in the event"}, 403
+
+        participant_to_update.role = Role.ADMIN
+        event.save()
+
+        return {"message": "Admin granted"}, 200
+
+
+class RevokeAdmin(Resource):
+    @classmethod
+    @jwt_required
+    def get(cls, event_id: str):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        data = parser.parse_args()
+
+        current_userid = get_jwt_identity()
+        current_user = UserModel.find_by_id(bson.ObjectId(current_userid))
+        if not current_user:
+            return {"message": "Current user not found"}, 403
+
+        user_to_update = UserModel.find_by_username(data['username'])
+        if not user_to_update:
+            return {"message": "User you want to add not found"}, 403
+        if user_to_update.id == current_user.id:
+            return {"message": "Cannot revoke admin to yourself"}, 403
+
+        event = EventModel.find_by_id_and_admin_id(bson.ObjectId(event_id), current_user.id)
+        if not event:
+            return {"message": "Event with admin as current user not found"}, 403
+
+        participant_to_update: ParticipantModel = next(
+            filter(lambda participant: participant.user_id == user_to_update.id, event.participants), None)
+        if not participant_to_update:
+            return {"message": "User you want to revoke admin is not in the event"}, 403
+
+        participant_to_update.role = Role.MEMBER
+        event.save()
+
+        return {"message": "Admin revoked"}, 200
