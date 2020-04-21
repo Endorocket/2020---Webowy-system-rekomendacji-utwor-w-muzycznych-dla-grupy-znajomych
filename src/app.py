f009d2cdd5e78deea5fbd1b4e6638128011bbed0
Endorocket <43682886+Endorocket@users.noreply.github.com>
2020-04-21 22:51:46 +0200

    Add cors

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from resources.event import Event, CreateEvent, EventList
 from resources.participant import InvitationByUsername, JoinByLink, RemoveUser, GrantAdmin, RevokeAdmin
 from resources.spotify_login import SpotifyLogin, SpotifyAuthorize
 from resources.user import UserRegister, User, UserLogin
+from flask_cors import CORS
 
 app = Flask(__name__)
 is_cloud = environ.get('IS_HEROKU', None)
@@ -23,20 +24,21 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = environ.get('APP_SECRET_KEY')
 api = Api(app)
 jwt = JWTManager(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-api.add_resource(UserRegister, "/register")
-api.add_resource(User, "/user/<user_id>")
-api.add_resource(UserLogin, "/login")
-api.add_resource(SpotifyLogin, "/login/spotify")
-api.add_resource(SpotifyAuthorize, "/login/spotify/authorized", endpoint="spotify.authorize")
-api.add_resource(Event, "/event/<event_id>")
-api.add_resource(EventList, "/events")
-api.add_resource(CreateEvent, "/event")
-api.add_resource(InvitationByUsername, "/event/<event_id>/invite")
-api.add_resource(RemoveUser, "/event/<event_id>/remove-user")
-api.add_resource(GrantAdmin, "/event/<event_id>/grant-admin")
-api.add_resource(RevokeAdmin, "/event/<event_id>/revoke-admin")
-api.add_resource(JoinByLink, "/join-event")
+api.add_resource(UserRegister, "/api/register")
+api.add_resource(User, "/api/user/<user_id>")
+api.add_resource(UserLogin, "/api/login")
+api.add_resource(SpotifyLogin, "/api/login/spotify")
+api.add_resource(SpotifyAuthorize, "/api/login/spotify/authorized", endpoint="spotify.authorize")
+api.add_resource(Event, "/api/event/<event_id>")
+api.add_resource(EventList, "/api/events")
+api.add_resource(CreateEvent, "/api/event")
+api.add_resource(InvitationByUsername, "/api/event/<event_id>/invite")
+api.add_resource(RemoveUser, "/api/event/<event_id>/remove-user")
+api.add_resource(GrantAdmin, "/api/event/<event_id>/grant-admin")
+api.add_resource(RevokeAdmin, "/api/event/<event_id>/revoke-admin")
+api.add_resource(JoinByLink, "/api/join-event")
 
 db.init_app(app)
 bcrypt.init_app(app)
