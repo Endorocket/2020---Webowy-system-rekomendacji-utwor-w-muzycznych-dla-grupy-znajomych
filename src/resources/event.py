eb811d146991c6208d4165dcca7557b5feef2cc6
Endorocket <43682886+Endorocket@users.noreply.github.com>
2020-05-11 05:04:35 +0200

    [PUT] /user/current

--- src/resources/event.py
+++ src/resources/event.py
@@ -29,6 +29,67 @@ class Event(Resource):
 
         return {"status": Status.OK, "event": event.json(users)}, 200
 
+    @classmethod
+    @jwt_required
+    def put(cls, event_id: str):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=False)
+        parser.add_argument('description', type=str, required=False)
+        parser.add_argument('start_date', type=datetime.fromisoformat, required=False)
+        parser.add_argument('end_date', type=datetime.fromisoformat, required=False)
+        parser.add_argument('duration_time', type=int, required=False)
+        parser.add_argument('image_url', type=str, required=False)
+        data = parser.parse_args()
+
+        if not ObjectId.is_valid(event_id):
+            return {"status": Status.INVALID_FORMAT, "message": "Id is not valid ObjectId"}, 400
+
+        current_userid = get_jwt_identity()
+        current_user = UserModel.find_by_id(current_userid)
+        if not current_user:
+            return {"status": Status.USER_NOT_FOUND, "message": "Current user not found"}, 403
+
+        event: EventModel = EventModel.find_by_id_and_admin_id(ObjectId(event_id), current_userid)
+        if not event:
+            return {"status": Status.NOT_FOUND, "message": "Event not found with admin as current user"}, 404
+
+        if data['name']:
+            if len(data['name']) < 3:
+                return {"status": Status.INVALID_FORMAT, "message": "name must be at least 3 chars long"}, 400
+            event.name = data['name']
+
+        if data['description']:
+            event.data = data['description']
+
+        start_date: datetime = data['start_date']
+        end_date: datetime = data['end_date']
+
+        if start_date and end_date:
+            if start_date > end_date:
+                return {"status": Status.INVALID_DATA, "message": "End_date cannot be before start_date"}, 403
+            event.start_date = start_date
+            event.end_date = end_date
+        elif end_date:
+            if event.start_date and event.start_date > end_date:
+                return {"status": Status.INVALID_DATA, "message": "End_date cannot be before start_date"}, 403
+            event.end_date = end_date
+        elif start_date:
+            if event.end_date and event.end_date < start_date:
+                return {"status": Status.INVALID_DATA, "message": "End_date cannot be before start_date"}, 403
+            event.start_date = start_date
+
+        duration_time = data['duration_time']
+        if duration_time and duration_time < 0:
+            return {"status": Status.INVALID_DATA, "message": "Duration_time cannot be less than 0"}, 403
+        event.duration_time = duration_time
+
+        if data['image_url']:
+            event.image_url = data['image_url']
+
+        event.save_to_db()
+
+        return {"status": Status.OK, "event": event.json()}, 200
+
 
 class EventList(Resource):
     @classmethod
