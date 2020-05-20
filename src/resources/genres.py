2ce172f3d008f92ec3b5b578ffe002d345698318
FlpWgda <flpwgda@gmail.com>
2020-05-20 02:09:16 +0200

    Add Status.INVALID_FORMAT

--- src/resources/genres.py
+++ src/resources/genres.py
@@ -1,9 +1,13 @@
 from flask_restful import Resource,reqparse
 from models.song import SongModel
+from enums.status import Status
 import json
 
 class TopGenres(Resource):
     @classmethod
     def get(cls, quantity: str):
-        top_genres = {'genres':[x['_id'] for x in SongModel.find_top_genres(int(quantity))]}
-        return top_genres
+        try:
+            top_genres = {'genres':[x['_id'] for x in SongModel.find_top_genres(int(quantity))]}
+            return top_genres
+        except ValueError:
+            return {"status": Status.INVALID_FORMAT, "message": "Value is not an integer"}, 400
